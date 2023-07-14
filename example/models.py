5472ef3ef0608c162de272df62e5cab1461d1f95
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-07-14 12:19:17 -0500

    Termine el primer video del tutorial

---
+++ example/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Simple(models.Model):
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
+
+    def __str__(self):
+        return self.url
+
+class DeteExample(models.Model):
+    the_date = models.DateTimeField()
+class NullExample(models.Model):
+    col = models.CharField(max_length=10, blank=True, null = True) 
+class Language(models.Model):
+    name = models.CharField(max_length=10) 
+class Framework(models.Model):
+    name = models.CharField(max_length=10) 
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)  
+# Create your models here.
